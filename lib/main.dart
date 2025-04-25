c270ddfb1e40069f25f975d971e13008c80e5f37
Newton Maluvana <andzanimaluvana@gmail.com>
2025-04-25 10:38:21 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Dating application", textDirection: TextDirection.ltr);
+  }
+}
